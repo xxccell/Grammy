2f6ae84cc14f71d80dfe4cc3459d6a9b259941cd
Axel <axeltanjingzhi@gmail.com>
2024-09-12 22:35:27 +0800

    Modified page elements and Improved code organisation
    Added page title and set page to auto wide. Increased height of input text area. Added loading spinner when doing conversion. Restructured code, removing LLM_response(). Use model.generate_content() instead of model.start_chat(). Added comments.

--- main.py
+++ main.py
@@ -2,19 +2,15 @@ import os
 import streamlit as st
 import google.generativeai as genai
 
+#Set up Gemini API key and Gemini Pro model
 GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
 genai.configure(api_key = GEMINI_API_KEY)
 model = genai.GenerativeModel("gemini-pro") 
 
-st.title("Grammy - Convert recipes to grams!")
-st.subheader("This takes the whole recipe and returns it back but everything is in grams now!")
-
-chat = model.start_chat()
-
-def LLM_Response(question):
-    response = chat.send_message(question, stream=True)
-    return response
-
+#Set up Streamlit page
+st.set_page_config(page_title="Grammy", layout="wide", initial_sidebar_state="auto")
+                   
+#Function for conversion to grams
 def convert_recipe_to_grams(recipe):
     prompt = f"""
     Convert the following recipe measurements from cups to grams and also millilitres to grams:
@@ -26,20 +22,24 @@ def convert_recipe_to_grams(recipe):
     After converting the ingredients, provide the complete recipe, including the directions.
     If no directions or instructions are given, do not generate any directions or instructions.
     """
-    result = LLM_Response(prompt)
-
+    result = model.generate_content(prompt)   #Generate the result using Gemini
+    
     converted_recipe = ""
     for word in result:
-        converted_recipe += word.text
+        converted_recipe += word.text   #Put together the generated text in one string
 
     return converted_recipe
 
-
-recipe = st.text_area("Paste recipe below to be converted")
+#Page UI
+st.title("Grammy - Convert recipes to grams!")
+st.subheader("This takes the whole recipe and returns it back but everything is in grams now!")
+recipe = st.text_area("Paste ingredients and recipe below to be converted",height=300 , placeholder="Ingredients and Recipe...")
 button = st.button("Convert to Grams")
 
-if recipe and button: 
-    result = convert_recipe_to_grams(recipe)
+#Perform conversion when user has entered a recipe and clicked the button
+if recipe and button:
+    with st.spinner("Converting..."):
+        result = convert_recipe_to_grams(recipe)
     st.subheader("Converted Recipe: ")
     st.markdown(result)
 
